f3ee98ea1012500b995dc7e8b8f34ab7b921c00a
SpyasDev <79065468+fastleon@users.noreply.github.com>
2024-05-06 17:03:05 -0500

    modificacion de URL para descarga de imagenes para acoplar su funcionamiento en el servidor

--- config/parametros_to_fill.php
+++ config/parametros_to_fill.php
@@ -1,15 +1,10 @@
 <?php
 
 //Estos son los parametros que se deben configurar para que funcione el modulo
+//llamar el archivo 
 
 define ('VE_DEBUG_MODE', true);  //-> modo debug?
-define ('VE_TIEMPO_RECARGA', 60); //segundos que guarda la ultima busqueda
-
-function getParametros() {
-    return array(
-        'base_url' => '',
-        'base_image_url' => '',
-        'service_name' => '',
-        'shipment_value' => '',
-    );
-}
+define ('VE_PRODUCCION', false);     // ambiente producción?
+define ('VE_TIEMPO_RECARGA', 60);   //segundos que guarda la ultima busqueda
+define ('VE_BASE_IMAGE_URL', '');       //URL desde donde llamar las imagenes
+define ('VE_PRODUCCION_URL', '');       //URL del dominio
