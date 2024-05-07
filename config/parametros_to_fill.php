190b83a6fdf9976969753e4e10cffaff7682e307
SpyasDev <79065468+fastleon@users.noreply.github.com>
2024-05-07 12:15:43 -0500

    correcciones en funcionalidad de boton Limpiar y modificacion para poder administrar quien puede usar este modulo.

--- config/parametros_to_fill.php
+++ config/parametros_to_fill.php
@@ -3,8 +3,8 @@
 //Estos son los parametros que se deben configurar para que funcione el modulo
 //llamar el archivo 
 
-define ('VE_DEBUG_MODE', true);  //-> modo debug?
-define ('VE_PRODUCCION', false);     // ambiente producción?
-define ('VE_TIEMPO_RECARGA', 60);   //segundos que guarda la ultima busqueda
-define ('VE_BASE_IMAGE_URL', '');       //URL desde donde llamar las imagenes
-define ('VE_PRODUCCION_URL', '');       //URL del dominio
+// define ('VE_DEBUG_MODE', true);  //-> modo debug?
+// define ('VE_PRODUCCION', false);     // ambiente producción?
+// define ('VE_TIEMPO_RECARGA', 60);   //segundos que guarda la ultima busqueda
+// define ('VE_BASE_IMAGE_URL', '');       //URL desde donde llamar las imagenes
+// define ('VE_PRODUCCION_URL', '');       //URL del dominio
